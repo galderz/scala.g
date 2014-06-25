8b259744cff3233d139ed1fb5daac698eeb73fea
Galder Zamarreño <galder@zamarreno.com>
2014-06-25 15:32:25 +0200

    Extractor examples

---
+++ src/main/scala/scalag/extractor/EmailExtractor.scala
@@ -0,0 +1,40 @@
+package scalag.extractor
+
+object EmailExtractor extends App {
+
+  def userTwiceUpper(s: String) = s match {
+    case Email(Twice(x @ UpperCase()), domain) =>
+      "match: "+ x +" in domain "+ domain
+    case _ =>
+      "no match"
+
+  }
+
+  println(userTwiceUpper("DIDI@hotmail.com"))
+  println(userTwiceUpper("DIDO@hotmail.com"))
+  println(userTwiceUpper("didi@hotmail.com"))
+
+  object Email {
+    // The injection method (optional)
+    def apply(user: String, domain: String) = user +"@"+ domain
+    // The extraction method (mandatory)
+    def unapply(str: String): Option[(String, String)] = {
+      val parts = str split "@"
+      if (parts.length == 2) Some(parts(0), parts(1)) else None
+    }
+  }
+
+  object Twice {
+    def apply(s: String): String = s + s
+    def unapply(s: String): Option[String] = {
+      val length = s.length / 2
+      val half = s.substring(0, length)
+      if (half == s.substring(length)) Some(half) else None
+    }
+  }
+
+  object UpperCase {
+    def unapply(s: String): Boolean = s.toUpperCase == s
+  }
+
+}
