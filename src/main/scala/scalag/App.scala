66ba8c3ca012370e2df7f6f080f7033683f5c893
Galder Zamarreño <galder@zamarreno.com>
2014-12-19 15:42:28 +0100

    Tests

--- src/main/scala/scalag/App.scala
+++ src/main/scala/scalag/App.scala
@@ -1,15 +1,35 @@
 package scalag
 
+import java.util.concurrent.ConcurrentHashMap
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
+  def foo1(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  def foo2(x : Array[String]) = x.foldRight("")((a,b) => a + b)
+
   def main(args : Array[String]) {
     println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+//    methodA(println(_))
+//    methodB(println("Boo"))
+//    methodA
+//    println("concat arguments (foldLeft)  = " + foo1(args))
+//    println("concat arguments (foldRight) = " + foo2(args))
+//
+//    val chm = new ConcurrentHashMap[Integer, String]()
+//    chm.put(1, "v1")
+//    println(chm.get(1))
+//    println(chm.replace(1, "v1", "v1"))
+  }
+
+  def methodA(x: String => Unit): Unit = {
+    x("boo")
+  }
+
+  def methodB(x: => Unit): Unit = {
+    x
   }
 
 }
